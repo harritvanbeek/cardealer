96bf7e78b469d424687ac29dd2b1977e6eb43059
boann <54663788+harritvanbeek@users.noreply.github.com>
2022-11-05 03:38:50 +0100

    adding install folder config file

---
+++ install/configuration.php
@@ -0,0 +1,12 @@
+<?php
+
+defined('_BOANN') or header("Location:{$_SERVER["REQUEST_SCHEME"]}://{$_SERVER["SERVER_NAME"]}");
+
+class Config{
+        public
+            $error_reporting    =   "none",
+            $host               =   "127.0.0.1",
+            $dbname             =   "your dbname",
+            $dbuser             =   "your userdbname",
+            $dbpassword         =   "your dbpassword";
+}
